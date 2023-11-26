46a490219d7b21a7141762bd4666ec821536ef37
Meghan Denny <hello@nektro.net>
2023-11-26 00:44:31 -0800

    fix tests for zig 0.11.0

--- build.zig
+++ build.zig
@@ -1,11 +1,14 @@
-const Builder = @import("std").build.Builder;
+const std = @import("std");
+const Builder = std.build.Builder;
 
 pub fn build(b: *Builder) void {
-    const mode = b.standardReleaseOptions();
-    var main_tests = b.addTest("src/all_test.zig");
-    main_tests.setBuildMode(mode);
+    const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
+    const main_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/all_test.zig" },
+        .optimize = mode,
+    });
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
-    b.default_step.dependOn(test_step);
+    const test_run = b.addRunArtifact(main_tests);
+    test_step.dependOn(&test_run.step);
 }
